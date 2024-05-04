26e909ae6e0146a86e8959f45f83d7b4f5c79f25
zfleisch <112609223+zfleisch@users.noreply.github.com>
2024-05-03 19:15:26 -0500

    Add to deck and deleting from deck working

--- backend/backend.js
+++ backend/backend.js
@@ -17,22 +17,70 @@ const dbName = "secoms319";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
+//You need to have a database named 'secoms319' in mongo with
+//2 collections names 'Mtg' and 'Deck'
+
 app.listen(port, () => {
     console.log("App listening at http://%s:%s", host, port);
 })
 
-app.get("/Cards/:id", async (req, res) => {
+app.get("/Cards/display/:color/:id", async (req, res) => {
     const range = Number(req.params.id);
+    const color = (req.params.color);
     await client.connect();
 
     console.log("Node successfully connected to GET Mongodb");
-
-    const query = { "id": {$gt: range}};
+    var query;
+    if(color === "all")
+    {
+        query = { "id": {$gt: range}};
+    }
+    else
+    {
+        query = { "id": {$gt: range}, colors: color};
+    }
 
     const results = await db
         .collection("Mtg")
         .find(query)
         .limit(20)
+        .toArray();
+
+    console.log(results);
+    res.status(200);
+    res.send(results);
+});
+
+app.get("/Cards/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    await client.connect();
+
+    console.log("Node successfully connected to GET Mongodb");
+
+    const query = { "id": id};
+
+    const results = await db
+        .collection("Mtg")
+        .find(query)
+        .limit(1)
+        .toArray();
+
+    console.log(results);
+    res.status(200);
+    res.send(results);
+});
+
+app.get("/Deck", async (req, res) => {
+    await client.connect();
+
+    console.log("Node successfully connected to GET Mongodb");
+
+    const query = {};
+
+    const results = await db
+        .collection("Deck")
+        .find(query)
+        .limit(100)
         .toArray();
 
     console.log(results);
@@ -50,7 +98,7 @@ app.post("/Deck", async (req, res) => {
         const newDocument = {
             "colors": req.body.colors,
             "identifiers": req.body.identifiers,
-            "id": req.body.id
+            "id": Number(req.body.id)
         };
         console.log(newDocument);
 
@@ -72,12 +120,25 @@ app.delete("/Deck/:id", async (req, res) => {
     try {
     const id = Number(req.params.id);
     await client.connect();
-    console.log("Store item to delete :", id);
+    console.log("Card to delete :", id);
     const query = { "id": id };
     // delete
     const itemDeleted = await db.collection("Deck").deleteOne(query);
     res.status(200);
     res.send(itemDeleted);
+    }
+    catch (error){
+    console.error("Error deleting item:", error);
+    res.status(500).send({ message: 'Internal Server Error' });
+    }
+});
+
+app.delete("/Deck", async (req, res) => {
+    try {
+    await client.connect();
+    await db.collection("Deck").deleteMany({});
+    res.status(200);
+    res.send("Collection Cleared")
     }
     catch (error){
     console.error("Error deleting item:", error);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,44 +2,256 @@ import React, {useState, useEffect} from "react";
 import { useForm } from "react-hook-form";
 import './App.css';
 
+//Global variables to aid in displaying cards
 var displayIndex = 0;
+var lowerBoundEstablished = false;
+var lowerBound;
+var filter = "all";
 
 function App() {
   const [cards, setCards] = useState([]);
   const [deck, setDeck] = useState([]);
+  const [viewer, setViewer] = useState(0);
+  //const [filter, setFilter] = useState("");
 
   useEffect(() =>{
     getCards();
   }, []);
 
   function getCards() {
-    fetch("http://localhost:8081/Cards/" + displayIndex)
+    fetch("http://localhost:8081/Cards/display/" + filter + "/" + displayIndex)
     .then(response => response.json())
     .then((data) => {
       console.log(data);
       setCards(data);
+      if(!lowerBoundEstablished)
+      {
+        lowerBound = data[0].id - 1;
+        displayIndex = lowerBound;
+        lowerBoundEstablished = true;
+      }
     });
   }
 
-  function addToDeck(id)
+  function addToDeck(object)
   {
+    if(deck.length == 100)
+    {
+      alert("Your deck is full");
+      return;
+    }
+    console.log(object);
+    var index = (object.id - 1) % 20;
+    console.log("Card at index");
+    console.log(cards[index]);
+    const deckPost = {
+      colors: cards[index].colors,
+      identifiers: cards[index].identifiers,
+      id: cards[index].id
+    };
+    console.log("POST Data");
+    console.log(deckPost);
+    fetch("http://localhost:8081/Deck", 
+    {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(deckPost)
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    alert("Card Added");
     return;
   }
 
   const showCards = cards.map((el) => (
     <div key={el.id}>
       <img src={el.identifiers.scryfallId} width={200} alt="images"/>
-      <button class="btn btn-green" onClick={addToDeck(el.id)}>Add to Deck</button>
+      <button key={el.id} class="btn btn-green" onClick={() => addToDeck(el)}>Add to Deck</button>
     </div>
   ));
 
-  return (
+  function deleteFromDeck(object)
+  {
+    const id = object.id;
+    console.log(id);
+    fetch("http://localhost:8081/Deck/" + id, 
+    {
+      method: 'DELETE'
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    alert("Card Removed");
+    getDeck();
+    return;
+  }
+
+  const showDeck = deck.map((el) => (
+    <div key={el.id}>
+      <img src={el.identifiers.scryfallId} width={200} alt="images"/>
+      <button key={el.id} class="btn btn-red" onClick={() => deleteFromDeck(el)}>Remove Card</button>
+    </div>
+  ));
+
+  function getDeck() {
+    fetch("http://localhost:8081/Deck")
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      setDeck(data);
+    });
+    setViewer(1);
+  }
+
+  const navBar = (
     <div>
-      <div class="flex flex-row">
-        {showCards}
-      </div>
+      <button class="btn-nav" onClick={() => setViewer(0)}>Home</button>
+      <button class="btn-nav" onClick={() => getDeck()}>Deck</button>
+      <button class="btn-nav" onClick={() => setViewer(2)}>Change Image</button>
+      <button class="btn-nav" onClick={() => setViewer(3)}>Student Information</button>
     </div>
   );
+
+  function changePage(i)
+  {
+    if(i === 0)
+    {
+      if((displayIndex - 20) >= lowerBound)
+      {
+        displayIndex = displayIndex - 20;
+        getCards();
+      }
+      else
+      {
+        alert("Viewing first page");
+        return;
+      }
+    }
+    else
+    {
+      if(cards.length === 20)
+      {
+        displayIndex = displayIndex + 20;
+        getCards();
+      }
+      else
+      {
+        alert("Viewing last page");
+        return;
+      }
+    }
+  }
+
+  const prevnext = (
+    <div>
+      <button class="btn-nav" onClick={() => changePage(0)}>Prev</button>
+      <button class="btn-nav" onClick={() => changePage(1)}>Next</button>
+    </div>
+  );
+
+  function deleteDeck()
+  {
+    fetch("http://localhost:8081/Deck", 
+    {
+      method: 'DELETE'
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    alert("Deck Cleared");
+    getDeck();
+    return;
+  }
+
+  const handleCheckBoxChange = (char) =>
+  {
+    filter = char;
+    lowerBoundEstablished = false;
+    displayIndex = 0;
+    getCards();
+    return;
+  }
+
+  if(viewer === 0)
+  {
+    return (
+      <div>
+        {navBar}
+        <div>
+          <label>
+            <input type="checkbox" checked={filter === "all"} onChange={() => handleCheckBoxChange("all")}/>
+            All Cards
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "W"} onChange={() => handleCheckBoxChange("W")}/>
+            White
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "B"} onChange={() => handleCheckBoxChange("B")}/>
+            Black
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "R"} onChange={() => handleCheckBoxChange("R")}/>
+            Red
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "U"} onChange={() => handleCheckBoxChange("U")}/>
+            Blue
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "G"} onChange={() => handleCheckBoxChange("G")}/>
+            Green
+          </label>
+        </div>
+        <div class="flex flex-row">
+          {showCards}
+        </div>
+        {prevnext}
+      </div>
+    );
+  }
+  else if(viewer === 1)
+  {
+    return (
+      <div>
+        {navBar}
+        <div>
+          <h1>Cards in deck: {deck.length}/100</h1>
+          <button class="btn btn-orange" onClick={() => deleteDeck()}>Clear Deck</button>
+        </div>
+        <div>
+          {showDeck}
+        </div>
+      </div>
+    );
+  }
+  else if(viewer === 2)
+  {
+    return (
+      <div>
+        {navBar}
+      </div>
+    );
+  }
+  else if(viewer === 3)
+  {
+    return (
+      <div>
+        {navBar}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
