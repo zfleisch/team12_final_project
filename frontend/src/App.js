6eb2f957450b33af01344fba19293fe70e23bbd7
JohnL13 <78700254+JohnL13@users.noreply.github.com>
2024-05-09 07:32:07 -0500

    Update App.js
    fixed Student info page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,173 +1,349 @@
-import './App.css';
 import React, {useState, useEffect} from "react";
-import {Products} from "./Products";
-import logo from './logo.png';
+import { useForm } from "react-hook-form";
+import './App.css';
+
+//Global variables to aid in displaying cards
+var displayIndex = 0;
+var lowerBoundEstablished = false;
+var lowerBound;
+var filter = "all";
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [deck, setDeck] = useState([]);
   const [viewer, setViewer] = useState(0);
+  const [updateImage, setUpdateImage] = useState(false);
+  const [updateCard, setUpdateCard] = useState([]);
+  const [newImage, setNewImage] = useState("");
 
-  const updateHooks = (e) => {
-    setViewer(e);
+  useEffect(() =>{
+    getCards();
+  }, []);
+
+  function getCards() {
+    fetch("http://localhost:8081/Cards/display/" + filter + "/" + displayIndex)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      setCards(data);
+      if(!lowerBoundEstablished)
+      {
+        lowerBound = data[0].id - 1;
+        displayIndex = lowerBound;
+        lowerBoundEstablished = true;
+      }
+    });
   }
 
-  function Cart() {
-    return (
-      <div>
-        Cart View
+  function addToDeck(object)
+  {
+    if(deck.length == 100)
+    {
+      alert("Your deck is full");
+      return;
+    }
+    console.log("Object print")
+    console.log(object);
+    var index = ((object.id - 1) - lowerBound) % 20;
+    console.log("Card at index");
+    console.log(cards[index]);
+    const deckPost = {
+      colors: cards[index].colors,
+      identifiers: cards[index].identifiers,
+      id: cards[index].id
+    };
+    console.log("POST Data");
+    console.log(deckPost);
+    fetch("http://localhost:8081/Deck", 
+    {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(deckPost)
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    alert("Card Added");
+    return;
+  }
+
+  const showCards = cards.map((el) => (
+    <div key={el.id}>
+      <img src={el.identifiers.scryfallId} width={200} alt="images"/>
+      <button key={el.id} class="btn btn-green" onClick={() => addToDeck(el)}>Add to Deck</button>
+    </div>
+  ));
+
+  function deleteFromDeck(object)
+  {
+    const id = object.id;
+    console.log(id);
+    fetch("http://localhost:8081/Deck/" + id, 
+    {
+      method: 'DELETE'
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    alert("Card Removed");
+    getDeck();
+    return;
+  }
+
+  const showDeck = deck.map((el) => (
+    <div key={el.id}>
+      <img src={el.identifiers.scryfallId} width={200} alt="images"/>
+      <button key={el.id} class="btn btn-red" onClick={() => deleteFromDeck(el)}>Remove Card</button>
+    </div>
+  ));
+
+  function getDeck() {
+    fetch("http://localhost:8081/Deck")
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      setDeck(data);
+    });
+    setViewer(1);
+  }
+
+  const navBar = (
+    <div>
+      <button class="btn-nav" onClick={() => setViewer(0)}>Home</button>
+      <button class="btn-nav" onClick={() => getDeck()}>Deck</button>
+      <button class="btn-nav" onClick={() => setViewer(2)}>Change Image</button>
+      <button class="btn-nav" onClick={() => setViewer(3)}>Student Information</button>
+    </div>
+  );
+
+  function changePage(i)
+  {
+    if(i === 0)
+    {
+      if((displayIndex - 20) >= lowerBound)
+      {
+        displayIndex = displayIndex - 20;
+        getCards();
+      }
+      else
+      {
+        alert("Viewing first page");
+        return;
+      }
+    }
+    else
+    {
+      if(cards.length === 20)
+      {
+        displayIndex = displayIndex + 20;
+        getCards();
+      }
+      else
+      {
+        alert("Viewing last page");
+        return;
+      }
+    }
+  }
+
+  const prevnext = (
+    <div>
+      <button class="btn-nav" onClick={() => changePage(0)}>Prev</button>
+      <button class="btn-nav" onClick={() => changePage(1)}>Next</button>
+    </div>
+  );
+
+  function deleteDeck()
+  {
+    fetch("http://localhost:8081/Deck", 
+    {
+      method: 'DELETE'
+    })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    alert("Deck Cleared");
+    getDeck();
+    return;
+  }
+
+  const handleCheckBoxChange = (char) =>
+  {
+    filter = char;
+    lowerBoundEstablished = false;
+    displayIndex = 0;
+    lowerBound = 0;
+    getCards();
+    return;
+  }
+
+  function updateSearch(id) {
+    console.log(id);
+    if(id >= 1 && id <= 449)
+    {
+      fetch("http://localhost:8081/Cards/" + id)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setUpdateCard(data);
+      })
+      setUpdateImage(true);
+    }
+    else
+    {
+      console.log("Index out of bounds")
+    }
+  }
+
+  function updateCardImage()
+    {
+      const inputCard = {
+        colors: updateCard[0].colors,
+        identifiers: {
+          scryfallId: newImage
+        },
+        id: updateCard[0].id,
+      };
+      console.log(updateCard);
+      fetch("http://localhost:8081/Cards/image/" + updateCard[0].id, 
+      {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(inputCard)
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      setUpdateImage(false);
+      setNewImage("");
+      alert("Image updated");
+      getCards();
+      setViewer(0);
+    }
+
+    const showUpdateCard = updateCard.map((el) => (
+      <div key={el.id}>
+        <img src={el.identifiers.scryfallId} width={200} alt="images"/>
       </div>
-    );
-  }
-  function Shop() {
-    const [cart, setCart] = useState([]);
-    const [cartTotal, setCartTotal] = useState(0);
-    const [query, setQuery] = useState('');
-    const [ProductsCategory, setProductsCategory] = useState(Products);
+    ));
 
     const handleChange = (e) => {
-      setQuery(e.target.value);
-      console.log("Step 6: in handleChange, Target value: ", e.target.value, " Query Value: ", query);
-      const results = Products.filter(eachProduct => {
-        if(e.target.value === "") return ProductsCategory;
-        return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase())
-      });
-      setProductsCategory(results);
+      setNewImage(e.target.value);
     }
 
-    useEffect(() => {
-        total();
-    }, [cart]);
-
-    const total = () => {
-        let totalVal = 0;
-        for(let i = 0; i < cart.length; i++) {
-            totalVal += cart[i].price;
-        }
-        setCartTotal(totalVal);
-    }
-
-    const addToCart = (el) => {
-        setCart([...cart, el]);
-    }
-
-    const removeFromCart = (el) => {
-      let itemFound = false;
-      const updatedCart = cart.filter((cartItem) => {
-        if(cartItem.id === el.id && !itemFound)
-        {
-          itemFound = true;
-          return false;
-        }
-        return true;
-      });
-        if(itemFound)
-        {
-          setCart(updatedCart);
-        }
-    }
-
-    const cartItems = cart.map((el) => (
-        <div key={el.id}>
-            <img class="img-fluid" src={el.image} width={150} />
-            {el.title}
-            ${el.price}
-        </div>
-    ));
-
-    function howManyofThis(id) {
-        let hmot = cart.filter((cartItem) => cartItem.id === id);
-        return hmot.length;
-    }
-
-    const listItems = ProductsCategory.map((el) =>(
-        // PRODUCT
-        <div class="row border-top border-bottom" key={el.id}>
-            <div class="group relative shadow-lg">
-                <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
-                    <img className="w-full h-full object-center object-cover lg:w-full lg:h-full" src={el.image} width={200}/>
-                </div>
-                <div className="flex justify-between p-3">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                    <div class="row text-muted"><strong>{el.title}</strong></div>
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500">Rating: {el.rating.rate}</p>
-                    <p className="text-sm font-medium text-green-600">${el.price}</p>
-                  </div>
-                </div>
-                <div class="col">
-                    <button class="bg-gray-500 text-white py-2 px-6 rounded-md" type="button" onClick={() => removeFromCart(el)} > - </button>{" "}
-                    <button class="bg-gray-500 text-white py-2 px-6 rounded-md" type="button" onClick={() => addToCart(el)}> + </button>
-                   
-                </div>
-            </div>
-        </div>
-    ));
-    return <div className="flex fixed flex-row">
-        <div className="h-screen  bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%' }}>
-          <img className="w-full" src={logo} alt="Sunset in the mountains" width={200}/>
-          <div className="px-6 py-4">
-            <h1 className="text-3xl mb-2 font-bold text-white"> Product Catalog App </h1>
-            <p className="text-gray-700 text-white">
-              by - <b style={{ color: 'orange' }}>Zak Fleischman, John Lavigne</b>
-            </p>
-            <div className="py-10">
-              <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
-  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
-  dark:focus:ring-blue-500 dark:focus:border-blue-500" type="search" value={query} onChange={handleChange}/>
-            </div>
-            <h3 className="text-lg text-gray-700">Items in cart: {cart.length}</h3>
-            <button class="bg-red-500 text-white py-2 px-6 rounded-md" onClick={() => updateHooks(1)}>CHECKOUT </button>
-          </div>
-        </div>
-        <div className="ml-5  p-10 xl:basis-4/5">
-          <div className='category-section fixed'>
-      <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products (6)</h2>
-      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll' }}>
-        {/* Loop Products */}
-        {listItems}
+    const updateButton = (
+      <div>
+        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
+dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+dark:focus:ring-blue-500 dark:focus:border-blue-500" type="search" value={newImage} onChange={handleChange}/>
+        <button onClick={() => updateCardImage()}>Change Image</button>
       </div>
-    </div>
-        </div>
-      </div>
-    
-    
-    
-    
-    
-    
-    
-    
-    ;
-  };
-  /*For activity 12 I did a return statement like: 
+    );
+
   if(viewer === 0)
   {
-    return (<div><Checkout/></div>)
+    return (
+      <div>
+        {navBar}
+        <div>
+          <label>
+            <input type="checkbox" checked={filter === "all"} onChange={() => handleCheckBoxChange("all")}/>
+            All Cards
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "W"} onChange={() => handleCheckBoxChange("W")}/>
+            White
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "B"} onChange={() => handleCheckBoxChange("B")}/>
+            Black
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "R"} onChange={() => handleCheckBoxChange("R")}/>
+            Red
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "U"} onChange={() => handleCheckBoxChange("U")}/>
+            Blue
+          </label>
+          <label>
+            <input type="checkbox" checked={filter === "G"} onChange={() => handleCheckBoxChange("G")}/>
+            Green
+          </label>
+        </div>
+        <div class="flex flex-row">
+          {showCards}
+        </div>
+        {prevnext}
+      </div>
+    );
   }
   else if(viewer === 1)
   {
-    return (<div><Shop/></div>)
+    return (
+      <div>
+        {navBar}
+        <div>
+          <h1>Cards in deck: {deck.length}/100</h1>
+          <button class="btn btn-orange" onClick={() => deleteDeck()}>Clear Deck</button>
+        </div>
+        <div>
+          {showDeck}
+        </div>
+      </div>
+    );
   }
-  etc...
-  */
- if(viewer === 0)
- {
-  return (
-    <div>
-        <Shop/>
-    </div>
+  else if(viewer === 2)
+  {
+    return (
+      <div>
+        {navBar}
+        <div>
+          <h1>Sometimes we get older images without knowing!!</h1>
+          <h2>Use this page to update a specific cards image to the most recent printing</h2>
+          <h3>Find card to update</h3>
+          <input type="text" id="message" name="message" placeholder="id" onChange={(e) => updateSearch(e.target.value)}/>
+          {updateImage && showUpdateCard}
+          {updateImage && updateButton}
+        </div>
+      </div>
     );
- }
- else if (viewer === 1)
- {
-  return (
-    <div>
-        <Cart/>
-    </div>
+  }
+  else if(viewer === 3)
+  {
+    return (
+      <div>
+        {navBar}
+        <div class="info-container">
+          <h3>Student Information</h3>
+          <p>Zak Fleischman - zfleisch@iastate.edu</p>
+          <p>John Lavigne - jlavigne@iastate.edu</p>
+          <h3>Course Information</h3>
+          <p>Iowa State University</p>
+          <p>Com S/SE 319</p>
+          <p>Construction of user interfaces</p>
+          <p>Professors: Ali Jannesari, Abraham Aldaco</p>
+          <h3>Date and Description</h3>
+          <p>May 9, 2024</p>
+          <p>Hello, this is a Com S 319 project that focuses on using a react native app in the front end and uses mongodb backend database access. The goal of this project is to refine and streamline the deck-building experience for Magic the Gathering players, providing a simpler alternative to Archidekt.com. By focusing on essential functionalities like card searching, filtering by attributes, and intuitive deck management, we aim to offer a more user-friendly platform for assembling and modifying decks.</p>
+        </div>
+      </div>
     );
- }
-
-} //end App
+  }
+  
+}
 
 export default App;
